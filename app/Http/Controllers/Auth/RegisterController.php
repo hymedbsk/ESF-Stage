2be7b349edb5da9a13abc5ee84db5f83fe7bc6d1
hymedbsk <45210165+hymedbsk@users.noreply.github.com>
2021-03-11 15:46:52 +0100

    Add files via upload

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'nom' => $data['name'],
             'prenom' =>$data['prenom'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
