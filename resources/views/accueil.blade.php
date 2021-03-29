45b204112f9dba9b89d925e849b0dc9e36f0b3f0
hymedbsk <45210165+hymedbsk@users.noreply.github.com>
2021-03-29 10:41:14 +0200

    Add files via upload

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -47,7 +47,6 @@
                 <ol class="carousel-indicators">
                   <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                   <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                 </ol>
                 <div class="carousel-inner">
                   <div class="carousel-item active">
@@ -56,9 +55,7 @@
                   <div class="carousel-item">
                     <img class="d-block w-100" src="{{asset('assets/img/arab.jpg')}}"alt="Second slide">
                   </div>
-                  <div class="carousel-item">
-                    <img class="d-block w-100" src="{{asset('assets/img/baby-84552_1920.jpg')}}" alt="Third slide">
-                  </div>
+
                 </div>
                 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -69,7 +66,6 @@
                   <span class="sr-only">Next</span>
                 </a>
             </div>
-
 
         </header>
         <!-- Services-->
