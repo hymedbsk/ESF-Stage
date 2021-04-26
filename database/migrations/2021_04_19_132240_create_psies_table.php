ecaf4c82e802aaaff97e8c36596b2557088c6a8f
hymedbsk <45210165+hymedbsk@users.noreply.github.com>
2021-04-26 11:56:47 +0100

    Add files via upload

---
+++ database/migrations/2021_04_19_132240_create_psies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePsiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('psy', function (Blueprint $table) {
+            $table->id('psy_id');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('specialite');
+            $table->string('photo');
+            $table->timestamp("date_cree");
+            $table->timestamp("date_edit")->nullable();
+            $table->timestamp("date_supp")->nullable();
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('psies');
+    }
+}
